5796e0b86b8118fd5bb95ebbe02bb59034087293
dduru26 <d.duru@alustudent.com>
2025-02-15 19:20:03 +0200

    Set functions for extracting urls

--- Regex - Onboarding Hackathon.js
+++ Regex - Onboarding Hackathon.js
@@ -2,3 +2,8 @@ function extractEmails(text) {
     const pattern = /\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b/g;
     return text.match(pattern);
 }
+
+function extractUrls(text) {
+    const pattern = /https?:\/\/(?:www\.)?\S+\.\S+/g;
+    return text.match(pattern);
+}
